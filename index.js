0271e25fdb70d10beaa8e0a0f454e5ef6cceb8a4
Serg <serg@MacBook-Pro-Serg.local>
2023-04-10 16:52:45 +0300

    update project

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ dotenv.config({path:'./config/.env'});
 
 const io = new Server(httpServer,{
     cors: {
-        origin: "https://main.dwpoabw8mgb51.amplifyapp.com",
+        origin: "https://spa-project-backend.vercel.app",
         methods: ["GET", "POST"],
     }
 });
